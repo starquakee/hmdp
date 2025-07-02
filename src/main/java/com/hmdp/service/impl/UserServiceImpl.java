d70dffe16a8f940ffd08ffa630c588deb7ae35df
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-07-02 21:49:30 +0800

    add login

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,20 +1,57 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import javax.servlet.http.HttpSession;
+
+
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误!");
+        }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code", code);
+        log.debug("发送短信成功，验证码为：{}", code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(loginForm.getPhone())){
+            return Result.fail("手机号格式错误!");
+        }
+        String code = (String) session.getAttribute("code");
+        if(code == null || !code.equals(loginForm.getCode())){
+            return Result.fail("验证码错误!");
+        }
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        if(user == null){
+            user = createUserWithPhone(loginForm.getPhone());
+        }
+        session.setAttribute("user", user);
+        return null;
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
