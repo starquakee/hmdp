43e74fd57de9e898c67968b6647ffec53fb85436
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-07-05 13:59:58 +0800

    add cache for shop-type list

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryTypeList() {
+        String key = "cache:shopTypeList";
+        String shopTypeListJson = stringRedisTemplate.opsForValue().get(key);
+        if(StrUtil.isNotBlank(shopTypeListJson)){
+            List<ShopType> list = JSONUtil.toList(shopTypeListJson, ShopType.class);
+            return Result.ok(list);
+        }
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+        return Result.ok(typeList);
+    }
 }
