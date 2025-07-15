8b9367adcfec71c1e88f0c506e27ac668facd6c7
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-07-15 13:40:03 +0800

    add UV test

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -68,6 +68,22 @@ class HmDianPingApplicationTests {
             stringRedisTemplate.opsForGeo().add(key, locations);
         }
     }
+    @Test
+    void testHyperLogLog() {
+        String[] values = new String[1000];
+        int j;
+        for (int i = 0; i < 100000; i++) {
+            j = i % 1000;
+            values[j] = "user_" + j;
+            if(j == 999){
+                // 发送到Redis
+                stringRedisTemplate.opsForHyperLogLog().add("hl2", values);
+            }
+        }
+        // 统计数量
+        Long count = stringRedisTemplate.opsForHyperLogLog().size("hl2");
+        System.out.println("count = " + count);
+    }
 
 
 }
